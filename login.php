5b6541a7da85aba0c703abc3a0db50e32fd4190a
DivFlopz <Divoflopz@gmail.com>
2021-06-15 01:55:08 -0500

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="https://bootswatch.com/5/vapor/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/sketchy/bootstrap.min.css" integrity="undefined" crossorigin="anonymous">
 </head>
 <body>
 <div class="container-fluid text-center">
