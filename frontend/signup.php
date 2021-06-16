7afa17704ad9aaf6a91141edd6332564cd149d3f
Digp33 <85522284+Digp33@users.noreply.github.com>
2021-06-16 12:10:50 -0500

    Update signup.php

--- frontend/signup.php
+++ frontend/signup.php
@@ -2,6 +2,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta name="author" content="Flores López Luis Divier, Hernández Cárdenas Daniel, González Portilla Daniel Iram,Vera Avelino Oscar">
+    <meta name="keywords" content="HTML, CSS, Cajas, Maquetado, Listas">
+    <meta name="copyright" content="Tzolkin">
+    <meta name="description" content="Singup de usuarios a la pagina">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
